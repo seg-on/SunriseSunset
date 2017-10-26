91d23ebec4a11c7f3c0e1b5ef37f6068aa5ba088
seg-on <tvanco@gmailc.om>
2017-10-26 14:39:38 +0200

    Initial

---
+++ SunriseSunset/SST.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+using System.Web.Script.Serialization;
+using System.Windows.Forms.VisualStyles;
+
+
+namespace SunriseSunset
+{
+    class SST
+    {
+        public GeoCoordinate locality;
+
+        public void Test()
+        {
+            locality = new GeoCoordinate();
+            //Piestany
+            locality.Latitude = 48.584167;
+            locality.Longitude = -17.833611;
+
+            //string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=48.584167&lng=-17.833611&date=2017-10-25");
+            string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=48.584167&lng=17.833611&date=2017-10-25");
+            Console.Write(json);
+            SSAPI piestanyInfo = new JavaScriptSerializer().Deserialize<SSAPI>(json);
+
+            Console.Write(piestanyInfo.results.PrintPropreties());
+            Console.Read();
+        }
+
+
+    }
+
+
+    public class SSAPI
+    {
+        public SunInfo results { get; set; }
+        public string status { get; set; }
+    }
+    public class SunInfo
+    {
+        public DateTime sunrise { get; set; }
+        public DateTime sunset { get; set; }
+        public DateTime solar_noon { get; set; }
+        public DateTime day_length { get; set; }
+        public DateTime civil_twilight_begin { get; set; }
+        public DateTime civil_twilight_end { get; set; }
+        public DateTime nautical_twilight_begin { get; set; }
+        public DateTime nautical_twilight_end { get; set; }
+        public DateTime astronomical_twilight_begin { get; set; }
+        public DateTime astronomical_twilight_end { get; set; }
+
+        public string PrintPropreties()
+        {
+            string allData = System.Environment.NewLine;
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(this))
+            {
+                string name = descriptor.Name;
+                string value = descriptor.GetValue(this).ToString();
+                allData += name + ":" + value.PadLeft(50- name.Length) +System.Environment.NewLine;
+            }
+            return allData;
+        }
+
+    }
+}
