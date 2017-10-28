fe3b3f89d09eac8a8eb246170f9e04c6bf0050b6
seg-on <tvanco@gmail.com>
2017-10-28 10:25:55 +0200

    Longitude and Latitude add as Geaocordinate

--- SunriseSunset/SST.cs
+++ SunriseSunset/SST.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Device.Location;
 using System.Web.Script.Serialization;
 using System.Windows.Forms.VisualStyles;
+using System.Globalization;
 
 
 namespace SunriseSunset
@@ -21,10 +22,14 @@ namespace SunriseSunset
             locality = new GeoCoordinate();
             //Piestany
             locality.Latitude = 48.584167;
-            locality.Longitude = -17.833611;
+            locality.Longitude = 17.833611;
 
+            string Today = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString();
             //string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=48.584167&lng=-17.833611&date=2017-10-25");
-            string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=48.584167&lng=17.833611&date=2017-10-25");
+            //string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=48.584167&lng=17.833611&date=2017-10-25");
+            string lat = locality.Latitude.ToString(CultureInfo.InvariantCulture);
+            string lon = locality.Longitude.ToString(CultureInfo.InvariantCulture);
+            string json = new WebClient().DownloadString("https://api.sunrise-sunset.org/json?lat=" + lat + "&lng="+lon+"&date=" + Today);
             Console.Write(json);
             SSAPI piestanyInfo = new JavaScriptSerializer().Deserialize<SSAPI>(json);
 
@@ -60,7 +65,8 @@ namespace SunriseSunset
             foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(this))
             {
                 string name = descriptor.Name;
-                string value = descriptor.GetValue(this).ToString();
+                DateTime dValue = Convert.ToDateTime(descriptor.GetValue(this));
+                string value = dValue.ToLocalTime().ToString();
                 allData += name + ":" + value.PadLeft(50- name.Length) +System.Environment.NewLine;
             }
             return allData;
